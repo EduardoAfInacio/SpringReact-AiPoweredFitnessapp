e029f0e59bfb5640c4f08e37563481ec4c5f0c04
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-18 23:10:02 -0300

    first commit

---
+++ AiPowereredFitnessApp-UserMicroService/src/main/java/com/fitness/AiPowereredFitnessApp_UserMicroService/AiPowereredFitnessAppUserMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.AiPowereredFitnessApp_UserMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPowereredFitnessAppUserMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPowereredFitnessAppUserMicroServiceApplication.class, args);
+	}
+
+}
---
+++ AiPowereredFitnessApp-UserMicroService/src/test/java/com/fitness/AiPowereredFitnessApp_UserMicroService/AiPowereredFitnessAppUserMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.AiPowereredFitnessApp_UserMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPowereredFitnessAppUserMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
