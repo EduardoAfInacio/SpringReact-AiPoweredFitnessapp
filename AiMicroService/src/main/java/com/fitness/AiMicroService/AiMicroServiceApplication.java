49e25413a12f5b7ffe437324370ea14d381f0c97
EduardoAfInacio <eduafinacio@gmail.com>
2025-10-27 18:18:21 -0300

    Setting up Ai micro service

---
+++ AiMicroService/src/main/java/com/fitness/AiMicroService/AiMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.AiMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMicroServiceApplication.class, args);
+	}
+
+}
