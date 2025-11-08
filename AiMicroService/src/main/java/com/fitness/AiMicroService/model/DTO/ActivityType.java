3408d0ebe5b9d39e5b0829457c2bf2e4d75a65d8
EduardoAfInacio <eduafinacio@gmail.com>
2025-11-08 11:07:48 -0300

    Setting up AI service to listen to RabbitMq activity queue and consume its messages.

---
+++ AiMicroService/src/main/java/com/fitness/AiMicroService/model/DTO/ActivityType.java
@@ -0,0 +1,21 @@
+package com.fitness.AiMicroService.model.DTO;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    WALKING,
+    WEIGHTLIFTING,
+    YOGA,
+    CARDIO,
+    STRETCHING,
+    PULLUP,
+    SQUAT,
+    BENCH_PRESS,
+    LEG_RAISE,
+    LUNGE,
+    BARBELL_SHOULDER_PRESS,
+    LEG_CURL,
+    HIP_THRUST,
+    HIP_EXTENSION,
+}
---
+++ AiMicroService/src/main/java/com/fitness/AiMicroService/service/ActivityMessageListener.java
@@ -0,0 +1,19 @@
+package com.fitness.AiMicroService.service;
+
+import com.fitness.AiMicroService.model.DTO.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+    private static final String queueName = "activity.queue";
+
+    @RabbitListener(queues = queueName)
+    public void activityMessageListener(Activity activity) {
+        log.info("Message received: {}", activity.toString());
+    }
+}
