33c23397d296a78335daedaba12c00e742fbda12
EduardoAfInacio <eduafinacio@gmail.com>
2025-11-28 17:19:22 -0300

    Created basic activity react components.

---
+++ react-frontend/src/components/ActivityDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ActivityDetail = () => {
+    return (
+        <div>Activity Details</div>
+    )
+}
+
+export default ActivityDetail;
---
+++ react-frontend/src/components/ActivityForm.jsx
@@ -0,0 +1,74 @@
+import { TextField } from '@mui/material';
+import {React, useState} from 'react';
+import { Button, Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+
+
+const ActivityForm = ({onActivityAdded}) => {
+
+    const [activity, setActivity] = useState({
+        type: "RUNNING",
+        duration: '',
+        caloriesBurned: '',
+        additionalMetrics: {}
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            //await addActivity(activity);
+            onActivityAdded();
+            setActivity({type: "RUNNING", duration: 0, caloriesBurned: ''});
+        }catch(error){
+            console.error("Error submitting activity:", error);
+        }
+    }
+
+    return (
+        <Box component='form' onSubmit={handleSubmit} sx={{ mb: 4 }}>
+            <FormControl fullWidth sx={{ mb: 2}}>
+                <InputLabel>Activity Type</InputLabel>
+                <Select
+                value={activity.type}
+                onChange={(e) => setActivity({ ...activity, type: e.target.value })}
+                >
+                    <MenuItem value="RUNNING">Running</MenuItem>
+                    <MenuItem value="CYCLING">Cycling</MenuItem>
+                    <MenuItem value="SWIMMING">Swimming</MenuItem>
+                    <MenuItem value="WALKING">Walking</MenuItem>
+                    <MenuItem value="WEIGHTLIFTING">Weightlifting</MenuItem>
+                    <MenuItem value="YOGA">Yoga</MenuItem>
+                    <MenuItem value="CARDIO">Cardio</MenuItem>
+                    <MenuItem value="STRETCHING">Stretching</MenuItem>
+                    <MenuItem value="PULLUP">Pull-Up</MenuItem>
+                    <MenuItem value="SQUAT">Squat</MenuItem>
+                    <MenuItem value="BENCH_PRESS">Bench Press</MenuItem>
+                    <MenuItem value="LEG_RAISE">Leg Raise</MenuItem>
+                    <MenuItem value="LUNGE">Lunge</MenuItem>
+                    <MenuItem value="BARBELL_SHOULDER_PRESS">Barbell Shoulder Press</MenuItem>
+                    <MenuItem value="LEG_CURL">Leg Curl</MenuItem>
+                    <MenuItem value="HIP_THRUST">Hip Thrust</MenuItem>
+                    <MenuItem value="HIP_EXTENSION">Hip Extension</MenuItem>
+                </Select>
+            </FormControl>
+            <TextField
+                fullWidth
+                label="Duration (minutes)"
+                type="number"
+                sx={{mb:2}}
+                value={activity.duration}
+                onChange={(e) => setActivity({...activity, duration: e.target.value})}/>
+            <TextField
+                fullWidth
+                label="Calories Burned"
+                type="number"
+                sx={{mb:2}}
+                value={activity.caloriesBurned}
+                onChange={(e) => setActivity({...activity, caloriesBurned: e.target.value})}/>
+            <Button type="submit" variant="contained">
+                Add Activity
+            </Button>
+        </Box>
+    )
+}
+
+export default ActivityForm;
