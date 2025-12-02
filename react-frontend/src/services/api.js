c076b77d96d777f77d76a149bc992751360b6a5e
EduardoAfInacio <eduafinacio@gmail.com>
2025-12-02 00:02:16 -0300

    Creating api endpoints to call backend from react.

---
+++ react-frontend/src/services/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:8080/api';
+
+const api = axios.create({
+    baseURL: API_URL,
+});
+
+api.interceptors.request.use((config) => {
+    const userId = localStorage.getItem('userId');
+    const token = localStorage.getItem('token');
+
+    if(token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    if(userId) {
+        config.headers['X-User-ID'] = userId;
+    }
+    return config;
+});
+
+export const getActivitiesByUser = () => api.get('/activities');
+export const getActivityRecommendation = (id) => api.get('/recommendations/activity/${id}', id);
+export const saveActivity = (activity) => api.post('/activities', activity);
